816a451326a44276f6349c1f08961e3b143e409a
datnguyex <nnguyendat72@gmail.com>
2024-10-17 13:36:10 +0700

    add new 2 file in folder front in branch front

---
+++ front/3068/3068-content.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3068 Interface</title>
+</head>
+<body>
+<div class="type-222">
+        <div class="container">
+            <div class="row">
+                <div class="col l-12">
+                    <h1 class="history__title">Our History</h1>
+                </div>
+                <div class="col l-12">
+                    <!-- Khoi ben trai -->
+                    <div class="history__blockLeft">
+                        <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>
+
+                        <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>
+
+                        <!-- Noi Dung cua khoi -->
+                        <div class="history__blockLeftParent">
+                            <div class="history__blockLeftDate">
+                                <p class="history__blockLeftText">2016</p>
+                            </div>
+                            <div class="history__blockLeftContent">
+                                <p class="history__blockLeftText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Khoi ben phai -->
+                    <div class="history__blockRight history__blockRightFirst">
+                       <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>   
+
+                             <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>  
+
+
+                             <!-- Noi Dung cua khoi -->
+                        <div class="history__blockRightParent">
+                            <div class="history__blockRightContent">
+                                <p class="history__blockRightText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                            <div class="history__blockRightDate">
+                                <p class="history__blockRightText">2015</p>
+                            </div>
+                        </div>
+                    </div>
+
+                         <!-- khoi ben trai -->
+                    <div class="history__blockLeft">
+                        <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>
+
+                        <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>
+
+                        <!-- Noi Dung cua khoi -->
+                        <div class="history__blockLeftParent">
+                            <div class="history__blockLeftDate">
+                                <p class="history__blockLeftText">2014</p>
+                            </div>
+                            <div class="history__blockLeftContent">
+                                <p class="history__blockLeftText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <!-- Khoi ben phai -->
+                    <div class="history__blockRight">
+                       <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>   
+
+                             <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>  
+
+
+                             <!-- Noi Dung cua khoi -->
+                        <div class="history__blockRightParent">
+                            <div class="history__blockRightContent">
+                                <p class="history__blockRightText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                            <div class="history__blockRightDate">
+                                <p class="history__blockRightText">2013</p>
+                            </div>
+                        </div>
+                    </div>
+
+                     <!-- khoi ben trai -->
+                     <div class="history__blockLeft">
+                        <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>
+
+                        <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>
+
+                        <!-- Noi Dung cua khoi -->
+                        <div class="history__blockLeftParent">
+                            <div class="history__blockLeftDate">
+                                <p class="history__blockLeftText">2012</p>
+                            </div>
+                            <div class="history__blockLeftContent">
+                                <p class="history__blockLeftText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                     <!-- Khoi ben phai -->
+                     <div class="history__blockRight">
+                       <!-- Thanh phan cach -->
+                        <div class="history__separator"></div>   
+
+                             <!-- Hinh tron -->
+                        <div class="history__circleWrapper">
+                            <div class="history__circle"></div>
+                        </div>  
+
+
+                             <!-- Noi Dung cua khoi -->
+                        <div class="history__blockRightParent">
+                            <div class="history__blockRightContent">
+                                <p class="history__blockRightText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo accusantium dignissimos delectus veniam dolore eaque soluta, quam nostrum. Iste obcaecati, repellat impedit soluta.
+                                </p>
+                            </div>
+                            <div class="history__blockRightDate">
+                                <p class="history__blockRightText">2011</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ front/3183/3183-content.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3183 Interface</title>
+</head>
+<body>
+    <div class='type-222'>
+        <div class='container'>
+            <div class='row'>
+                <div class='col l-6 m-4 m-6 c-12 c-12'>
+                    <div class='fotter__wrapFind'>
+                        <h4 class='footer__find'>
+                            <a class="footer__findLink" href="">FIND A STORE</a>
+                        </h4>
+                        <h4 class='footer__find'>
+                            <a class="footer__findLink" href="">SING UP FOR EMAIL</a>
+                        </h4>
+                        <h4 class='footer__find'>
+                            <a class="footer__findLink" href="">BECOME A MEMEBER</a>
+                        </h4>
+                        <h4 class='footer__find footer__findLinkLast'>
+                            <a class="footer__findLink" href="">STUDENT DISCOUNT</a>
+                        </h4>
+                    </div>
+                </div>
+                <div class='col l-6 m-4 m-6 c-12'>
+                    <div class='fotter__wrapFind'>
+                        <h4 class='footer__find'>
+                            <a class="footer__findLink" href="">SERVICE</a>
+                        </h4>
+                        <img class='footer_findImg' src="./img/footer-cards.png" alt="">
+                    </div>
+                </div>
+                <div class='col l-6 m-4 m-6 c-12'>
+                    <div class='fotter__wrapFind'>
+                        <h4 class='footer__service footer_serviceFirst'><a class='footer__findLink' href="">SERVICES</a></h4>
+                        <h4 class='footer__service footer_serviceSecond footer_animate'><a class='footer__serviceLink' href="">Carears</a></h4>
+                        <h4 class='footer__service footer_animate'><a class='footer__serviceLink' href="">Istore finder</a></h4>
+                        <h4 class='footer__service footer_animate'><a class='footer__serviceLink' href="">Account</a></h4>
+                        <h4 class='footer__service footer_serviceLast footer_animate'><a class='footer__serviceLink' href="">Contact Us</a></h4>
+                    </div>
+                </div>
+                <div class='col l-6 m-4 m-6 c-12'>
+                    <div class='fotter__wrapFind'>
+                        <h4 class='footer__service footer_serviceFirst'><a class='footer__findLink' href="">FIND A STORE</a></h4>
+                        <h4 class='footer__service footer_serviceSecond footer_animate'><a class='footer__serviceLink' href="">Order Status</a></h4>
+                        <h4 class='footer__service'><a class='footer__serviceLink footer_animate' href="">Delivery</a></h4>
+                        <h4 class='footer__service'><a class='footer__serviceLink footer_animate' href="">Returns</a></h4>
+                        <h4 class='footer__service'><a class='footer__serviceLink footer_animate' href="">Payment Options</a></h4>
+                        <h4 class='footer__service footer_serviceLast'><a class='footer__serviceLink footer_animate' href="">Contact Us</a></h4>
+                    </div>
+                </div>
+                <div class='col l-6 m-4 m-6 c-12'>
+                    <div class='fotter__wrapFind'>
+                        <h4 class='footer__service footer_serviceFirst'><a class='footer__findLink' href="">NEWS ABOUT</a></h4>
+                        <h4 class='footer__service footer_serviceSecond footer_animate'><a class='footer__serviceLink' href="">Carears</a></h4>
+                        <h4 class='footer__service'><a class='footer__serviceLink footer_animate' href="">Investor</a></h4>
+                        <h4 class='footer__service'><a class='footer__serviceLink footer_animate' href="">Sustainable Innovation</a></h4>
+                        <h4 class='footer__service footer_serviceLast'><a class='footer__serviceLink footer_animate' href="">UK Modern Slavery Act</a></h4>
+                    </div>
+                </div>
+                <div class='col l-6 m-4 m-6 c-12'>
+                  <div class='fotter__wrapFind'>
+                        <h4 class='footer__service footer_serviceFirst'><a class='footer__findLink' href="">LET'S CONNECT</a></h4>
+                        <img class='footer__listSocialMedia footer_serviceSecond' src="./img/socialmedia.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='container'>
+            <div class='row'>   
+                     <div class='col l-2 c-12 '>
+                       <div class='footer__wrapEnd footer_endLeft'>
+                            <p class='footer-endLight'> © 2017</p>
+                            <p class='footer-endSpace'>.</p>
+                            <p class='footer-endDark'>Qode Interactive,</p>
+                            <p class='footer-endSpace'>.</p>
+                            <p class='footer-endLight'> All Rights Reserved</p>
+                       </div>
+                    </div>
+                    <div class='col l-2 c-12'>
+                        <div class='footer__wrapEnd footer_endRight'>
+                            <p class='footer-endLight'>Made With Love by</p>
+                            <p class='footer-endSpace'>.</p>
+                            <p class='footer-endDark'> Qode Interactive</p>
+                        </div>
+                    </div>
+                
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
